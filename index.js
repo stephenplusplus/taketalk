1e4f0f1bed11e48a03968d412161ca422b0cdd64
Stephen Sawchuk <sawchuk@gmail.com>
2014-06-16 12:18:36 -0400

    taketalk takeone.

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var argv = require('minimist')(process.argv.slice(2));
+var stdin = require('get-stdin');
+var input = argv._[0];
+
+var taketalk = {
+  init: function () {},
+  printHelp: function () { this._doWhatItIs('help'); },
+  printVersion: function () { this._doWhatItIs('version'); },
+  _doWhatItIs: function (key) {
+    var thing = this[key];
+    switch (typeof thing) {
+      case 'string': console.log(thing); break;
+      case 'function': thing(); break;
+    }
+  }
+};
+
+module.exports = function (obj) {
+  if ({}.toString.call(obj).indexOf('Object') === -1) {
+    throw new Error('Expected an object.');
+  }
+
+  for (var prop in obj) {
+    taketalk[prop] = obj[prop];
+  }
+
+  if (argv.help || argv.h) return taketalk.printHelp();
+  if (argv.version || argv.v) return taketalk.printVersion();
+
+  if (process.stdin.isTTY) {
+    if (!input) {
+      taketalk.printHelp();
+      return;
+    }
+
+    taketalk.init(input, argv);
+  } else {
+    stdin(function (data) {
+      taketalk.init(data, argv);
+    });
+  }
+
+  return taketalk;
+};
